03f42de9446b45c262f8a347d7a34b659a59ab18
friteuseb <cyril.wolfangel@gmail.com>
2024-07-31 23:47:27 +0200

    Préparation pour la release 1.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'cyril.wolfangel@gmail.com',
     'state' => 'beta',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.2',
+    'version' => '1.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
-            'dashboard' => '12.4.0-12.4.99',
+            'typo3' => '12.4.0-12.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
