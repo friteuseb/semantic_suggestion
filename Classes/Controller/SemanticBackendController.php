bed04d780fb24fa4084d50e7958d4390e6a91bb0
friteuseb <cyril.wolfangel@gmail.com>
2024-08-02 15:58:51 +0200

    module backend v1.2

--- Classes/Controller/SemanticBackendController.php
+++ Classes/Controller/SemanticBackendController.php
@@ -2,7 +2,6 @@
 namespace TalanHdf\SemanticSuggestion\Controller;
 
 use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Backend\Template\ModuleTemplate;
 use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use TalanHdf\SemanticSuggestion\Service\PageAnalysisService;
@@ -28,32 +27,84 @@ class SemanticBackendController extends ActionController
     {
         $moduleTemplate = $this->moduleTemplateFactory->create($this->request);
 
-        // Récupérer la configuration TypoScript complète
         $fullTypoScript = $this->configurationManager->getConfiguration(
             ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT
         );
 
-        // Extraire la configuration spécifique à notre extension
         $extensionConfig = $fullTypoScript['plugin.']['tx_semanticsuggestion_suggestions.']['settings.'] ?? [];
 
         $parentPageId = (int)($extensionConfig['parentPageId'] ?? 0);
         $depth = (int)($extensionConfig['recursive'] ?? 1);
         $proximityThreshold = (float)($extensionConfig['proximityThreshold'] ?? 0.5);
         $maxSuggestions = (int)($extensionConfig['maxSuggestions'] ?? 5);
-        $analyzedFields = $extensionConfig['analyzedFields.'] ?? [];
 
         $analysisResults = $this->pageAnalysisService->analyzePages($parentPageId, $depth);
+
+        $statistics = $this->calculateStatistics($analysisResults, $proximityThreshold);
 
         $moduleTemplate->assignMultiple([
             'parentPageId' => $parentPageId,
             'depth' => $depth,
             'proximityThreshold' => $proximityThreshold,
             'maxSuggestions' => $maxSuggestions,
-            'analyzedFields' => $analyzedFields,
+            'statistics' => $statistics,
             'analysisResults' => $analysisResults,
         ]);
 
         $moduleTemplate->setContent($this->view->render());
         return $moduleTemplate->renderResponse();
     }
+
+
+    private function calculateStatistics(array $analysisResults, float $proximityThreshold): array
+{
+    $totalPages = count($analysisResults);
+    $totalSimilarityScore = 0;
+    $similarityPairs = [];
+    $distributionScores = [
+        '0.0-0.2' => 0, '0.2-0.4' => 0, '0.4-0.6' => 0, '0.6-0.8' => 0, '0.8-1.0' => 0
+    ];
+    $pagesSimilarityCount = [];
+
+    foreach ($analysisResults as $pageId => $pageData) {
+        $pagesSimilarityCount[$pageId] = 0;
+        foreach ($pageData['similarities'] as $similarPageId => $similarity) {
+            if ($pageId < $similarPageId) { // Évite les doublons
+                $totalSimilarityScore += $similarity['score'];
+                $similarityPairs[] = [
+                    'page1' => $pageId,
+                    'page2' => $similarPageId,
+                    'score' => $similarity['score']
+                ];
+                
+                if ($similarity['score'] >= $proximityThreshold) {
+                    $pagesSimilarityCount[$pageId]++;
+                    $pagesSimilarityCount[$similarPageId] = ($pagesSimilarityCount[$similarPageId] ?? 0) + 1;
+                }
+
+                // Mettre à jour la distribution des scores
+                if ($similarity['score'] < 0.2) $distributionScores['0.0-0.2']++;
+                elseif ($similarity['score'] < 0.4) $distributionScores['0.2-0.4']++;
+                elseif ($similarity['score'] < 0.6) $distributionScores['0.4-0.6']++;
+                elseif ($similarity['score'] < 0.8) $distributionScores['0.6-0.8']++;
+                else $distributionScores['0.8-1.0']++;
+            }
+        }
+    }
+
+    // Trier les paires par score de similarité décroissant
+    usort($similarityPairs, function($a, $b) {
+        return $b['score'] <=> $a['score'];
+    });
+
+    return [
+        'totalPages' => $totalPages,
+        'averageSimilarity' => $totalPages > 1 ? $totalSimilarityScore / (($totalPages * ($totalPages - 1)) / 2) : 0,
+        'topSimilarPairs' => array_slice($similarityPairs, 0, 5),
+        'distributionScores' => $distributionScores,
+        'topSimilarPages' => arsort($pagesSimilarityCount) ? array_slice($pagesSimilarityCount, 0, 5, true) : [],
+    ];
+}
+
+
 }
---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'dependencies' => ['backend'],
+    'imports' => [
+        '@typo3/semantic-suggestion/' => 'EXT:semantic_suggestion/Resources/Public/JavaScript/',
+    ],
+];
