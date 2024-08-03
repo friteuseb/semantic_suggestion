d4f1561fa08d1d9f12b167ab210b1872560eaeb6
friteuseb <cyril.wolfangel@gmail.com>
2024-08-03 09:20:48 +0200

    Update compatibility for TYPO3 12 and 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.4',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99'
+            'typo3' => '12.0.0-13.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
